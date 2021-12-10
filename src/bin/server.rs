ca4632296480c123dde38bb9a466fae4919632f3
Mark Meliksetyan <markmelix@gmail.com>
2021-12-10 22:45:02 +0300

    Fix: solved a lot of errors

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -62,7 +62,7 @@ fn main() {
 		None => GameData::RECOMMENDED_APPLES_AMOUNT,
 	};
 
-	let address = format!("127.0.0.1:{}", port);
+	let address = format!("0.0.0.0:{}", port);
 
 	println!("Running server on {} address", address);
 
